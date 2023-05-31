68dc59e39eac4edf84ef51a9402b2e4e8d31ce48
Alejandro Perez <perez.cousirat.alejandro@gmail.com>
2023-05-31 01:03:37 -0300

    create forms en home:autor y post

--- project/apps/home/urls.py
+++ project/apps/home/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("", views.index, name="index"),
+    path("crear-autor/", views.crear_autor, name="crear-autor"), 
+    path("crear-post/", views.crear_autor, name="crear-post"), 
 ]
